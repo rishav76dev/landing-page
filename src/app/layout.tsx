9ad0612d247908c2a03a9c4cb0a4b1622505ddc8
rishav76dev <rjrishav037@gmail.com>
2025-07-14 14:37:44 +0530

    icon changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,7 @@ import clsx from "clsx";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "Saas Landing Page",
 };
 
 export default function RootLayout({
