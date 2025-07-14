7dc475f9ce3da0f7098e69d98812e1e9b3da713f
rishav76dev <rjrishav037@gmail.com>
2025-07-14 07:30:15 +0530

    animate call to action

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,34 +1,57 @@
+"use client";
 import ArrowRight from "@/assets/arrow-right.svg";
 import starImage from "@/assets/star.png";
 import springImage from "@/assets/spring.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
-import Image from "next/image";
 export const CallToAction = () => {
-  return <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
-    <div className="container">
-      <div className="section-heading relative">
-      <h2 className="section-title">Sign up for free today</h2>
-      <p className="section-description mt-5">
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+  return (
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip"
+    >
+      <div className="container">
+        <div className="section-heading relative">
+          <h2 className="section-title">Sign up for free today</h2>
+          <p className="section-description mt-5">
             Celebrate the joy of accomplishment with an app designed to track
             your progress and motivate your efforts.
-      </p>
-      <Image src={starImage} alt="" width={360} className="absolute -left-[350px] -top-[17px]" />
-      <Image
-        src={springImage.src}
-        alt="Spring Image"
-        width={360}
-        height={360}
-        className="absolute -right-[331px] -top-[19px]"
-      />
-    </div>
-    </div>
-
-    <div className="flex gap-2 mt-10 justify-center">
-        <button className="btn btn-primary">Get for free</button>
-        <button className="btn btn-text gap-1">
-          <span>Learn more</span>
-          <ArrowRight className="h-5 w-5" />
-        </button>
-    </div>
-  </section>
+          </p>
+          <motion.img
+            src={starImage.src}
+            alt="Star Image"
+            width={360}
+            className="absolute -left-[350px] -top-[137px]"
+            style={{
+              translateY,
+            }}
+          />
+          <motion.img
+            src={springImage.src}
+            alt="Spring Image"
+            width={360}
+            className="absolute -right-[331px] -top-[19px]"
+            style={{
+              translateY,
+            }}
+          />
+        </div>
+        <div className="flex gap-2 mt-10 justify-center">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text gap-1">
+            <span>Learn more</span>
+            <ArrowRight className="h-5 w-5" />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
